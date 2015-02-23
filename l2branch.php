bca7435ac300c542bbca02f5832d9b2ff0f869cb
StanleyLu <x8892965@yahoo.com.tw>
2015-02-23 15:08:11 +0800

    merge test2

--- l2branch.php
+++ l2branch.php
@@ -5,7 +5,8 @@ class User{
 	public function __construct(){
 		
 		$testL2 = "L2建立的Branch";	//	from local to server 
-		$test = "這是L2建立的Branch";	//	測試衝突 
+		$test = "這是L2建立的Branch";	//	測試衝突
+		$test = "這是L2建立的Branch測試合併";	//	測試合併
 		
 	}
 	
