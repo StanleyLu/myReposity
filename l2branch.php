5e2743781621e30536b5e8fdae1d9a3b0c35bdcb
StanleyLu <x8892965@yahoo.com.tw>
2015-02-23 14:59:06 +0800

    commit l2

---
+++ l2branch.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+class User{
+
+	public function __construct(){
+		
+		$testL2 = "L2建立的Branch";	//	from local to server 
+		
+	}
+	
+	
+	public function get_user_ip(){
+		if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+			$ip = $_SERVER['HTTP_CLIENT_IP'];
+		} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+			$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+		} elseif (!empty($_SERVER['HTTP_X_FORWARDED'])) {
+			$ip = $_SERVER['HTTP_X_FORWARDED'];
+		} elseif (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])) {
+			$ip = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+		} elseif (!empty($_SERVER['HTTP_FORWARDED_FOR'])) {
+			$ip = $_SERVER['HTTP_FORWARDED_FOR'];
+		} elseif (!empty($_SERVER['HTTP_FORWARDED'])) {
+			$ip = $_SERVER['HTTP_FORWARDED'];
+		} else {
+			$ip = $_SERVER['REMOTE_ADDR'];
+		}
+		return $ip;
+	}
+
+}
+
+
+?>
