5989ca351fe4d9684d081a9a662f7917c928297e
StanleyLu <x8892965@yahoo.com.tw>
2015-02-23 14:27:29 +0800

    Revert "Delete user.php"
    This reverts commit 88e1be5b76d37b223bd3394b967127a72b3f66c5.

---
+++ user.php
@@ -0,0 +1,33 @@
+ï»¿<?php
+
+class User{
+
+	public function __construct(){
+		$test = "±qlocal¤W¶Ç§ïUtf8";	//	from local to server 
+		
+	}
+	
+	
+	public function get_user_ip(){
+		if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+			$ip = $_SERVER['HTTP_CLIENT_IP'];
+		} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+			$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+		} elseif (!empty($_SERVER['HTTP_X_FORWARDED'])) {
+			$ip = $_SERVER['HTTP_X_FORWARDED'];
+		} elseif (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])) {
+			$ip = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+		} elseif (!empty($_SERVER['HTTP_FORWARDED_FOR'])) {
+			$ip = $_SERVER['HTTP_FORWARDED_FOR'];
+		} elseif (!empty($_SERVER['HTTP_FORWARDED'])) {
+			$ip = $_SERVER['HTTP_FORWARDED'];
+		} else {
+			$ip = $_SERVER['REMOTE_ADDR'];
+		}
+		return $ip;
+	}
+
+}
+
+
+?>
